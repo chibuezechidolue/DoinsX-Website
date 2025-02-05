c7b9807112e75d9e661a50b0c85ad85bce2cdc01
Stanley Chidolue <chidoluechibueze@gmail.com>
2025-02-05 07:30:34 +0100

    installed django-cleaup to automatically delete an previously uploaded file from the backend or external storage

--- Website/models.py
+++ Website/models.py
@@ -6,7 +6,7 @@ import uuid
 
 class TalentCategories(models.Model):
     category_title=models.CharField(max_length=200,unique=True)
-    category_img=models.FileField(upload_to=f"images/categories",default="category-default.jpg")
+    category_img=models.FileField(upload_to=f"media/categories",default="category-default.jpg")
 
 
     def __str__(self):
@@ -17,18 +17,18 @@ class Talent(models.Model):
     id=models.UUIDField(default=uuid.uuid4,primary_key=True)
     first_name=models.CharField(max_length=150,null=False)
     last_name=models.CharField(max_length=150,null=False)
-    other_names=models.CharField(max_length=300,null=True)
+    other_names=models.CharField(max_length=300,null=True, blank=True)
     profession=models.CharField(max_length=300,null=False)
     product_gender=models.CharField(max_length=10,choices=(
         ('MALE', u'MALE'),
         ('FEMALE', u'FEMALE'),
     ),null=False)
-    stage_name=models.CharField(max_length=200,null=True)
-    height=models.CharField(max_length=20,null=True)
+    stage_name=models.CharField(max_length=200,null=True, blank=True)
+    height=models.CharField(max_length=20,null=True, blank=True)
     about=models.TextField()
-    image1=models.FileField(upload_to=f"images/categories",default="talent-default.jpg",null=False)
-    image2=models.FileField(upload_to=f"images/categories",null=False)
-    image3=models.FileField(upload_to=f"images/categories",null=False)
+    image1=models.FileField(upload_to=f"media/talents",default="talent-default.jpg",null=False)
+    image2=models.FileField(upload_to=f"media/talents",null=True, blank=True)
+    image3=models.FileField(upload_to=f"media/talents",null=True, blank=True)
     category = models.ForeignKey(TalentCategories, on_delete=models.CASCADE,)
 
 
@@ -37,11 +37,11 @@ class Talent(models.Model):
     
 
 class Advertisement(models.Model):
-    image1=models.FileField(upload_to=f"images/advert",default="advert-default.jpg",null=True)
-    image2=models.FileField(upload_to=f"images/advert",null=False)
-    image3=models.FileField(upload_to=f"images/advert",null=False)
-    image4=models.FileField(upload_to=f"images/advert",null=False)
-    image5=models.FileField(upload_to=f"images/advert",null=False)
+    image1=models.FileField(upload_to=f"media/advert",default="advert-default.jpg",null=False)
+    image2=models.FileField(upload_to=f"media/advert",null=True, blank=True)
+    image3=models.FileField(upload_to=f"media/advert",null=True, blank=True)
+    image4=models.FileField(upload_to=f"media/advert",null=True, blank=True)
+    image5=models.FileField(upload_to=f"media/advert",null=True, blank=True)
 
     def __str__(self):
 	    return "Advertisement"
